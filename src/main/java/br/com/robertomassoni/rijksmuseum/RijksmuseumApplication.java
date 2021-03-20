115d307b7e1834023eaaed7d950ea4466d49e8fb
betomassoni <36550944+betomassoni@users.noreply.github.com>
2021-03-20 10:33:36 -0300

    Initial commit

---
+++ src/main/java/br/com/robertomassoni/rijksmuseum/RijksmuseumApplication.java
@@ -0,0 +1,13 @@
+package br.com.robertomassoni.rijksmuseum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RijksmuseumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RijksmuseumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/robertomassoni/rijksmuseum/RijksmuseumApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.robertomassoni.rijksmuseum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RijksmuseumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
